1cb6d20d5e5166e92ec7bb4ed522295ef4d6bcaf
Gokalp Koksal <gokalp.koksal4@gmail.com>
2022-04-15 22:13:00 +0300

    Add timer in splashVC to navigate searchMovieVC

--- LoodosMovieBox/Scenes/Splash/SplashScreenViewController.swift
+++ LoodosMovieBox/Scenes/Splash/SplashScreenViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class SplashScreenViewController: UIViewController, SplashScreenDelegate {
     
     var viewModel = SplashScreenViewModel()
+    var timer: Timer = Timer()
+    var counter = 0
 
     @IBOutlet weak var loodosLabel: UILabel!
     
@@ -24,5 +26,24 @@ class SplashScreenViewController: UIViewController, SplashScreenDelegate {
     
     func updateLogoText(text: String) {
         loodosLabel.text = text
+        startTimer()
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(threeSecondsCounter), userInfo: nil, repeats: false)
+    }
+    
+    @objc func threeSecondsCounter() {
+        counter = counter + 1
+        if counter == 3 {
+            timer.invalidate()
+            navigateToSearchMovieController()
+        }
+    }
+    
+    private func navigateToSearchMovieController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "SearchMovieViewController") as! SearchMovieViewController
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
